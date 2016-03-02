bf3b8f2e14d2b8c3b5a2c87380d4e0cdbeea29f2
lowitty <perfectbeauty@qq.com>
2016-03-02 14:12:40 +0800

    line startswith # will be comments. Will not strip the properties.

--- PyProperties.py
+++ PyProperties.py
@@ -1,46 +1,44 @@
-#encoding=utf-8
-'''
-Created on Aug 24, 2015
-
-@author: lowitty
-'''
+# -*- coding: utf-8 -*-
 import os
-class Properties():
-    
-    def __init__(self, propertyFile):
-        self.path = os.path.normpath(propertyFile)
-        if(not os.path.isfile(self.path)):
+
+
+class Properties:
+    def __init__(self, prop_file, is_strip=False):
+        self.path = os.path.normpath(prop_file)
+        if not os.path.isfile(self.path):
             raise IOError("The path that you specified is not a file.")
         else:
-            self.file = open(self.path, 'rb')
+            self.file = open(self.path, 'r+')
             lines = self.file.readlines()
             self.mappings = {}
             for line in lines:
-                keyValue = line.split('=')
-                if(len(keyValue) > 1):
-                    self.mappings[keyValue[0]] = keyValue[1].rstrip('\r').rstrip('\n')
+                if not line.lstrip().startswith('#'):
+                    key_value = line.split('=')
+                    if 1 < len(key_value):
+                        if is_strip:
+                            self.mappings[key_value[0]] = key_value[1].rstrip('\r').rstrip('\n')
+                        else:
+                            self.mappings[key_value[0]] = key_value[1]
             self.file.close()
-                    
-    def getProperty(self, key):
-        if(self.mappings.has_key(key)):
+
+    def get_property(self, key):
+        if self.mappings.has_key(key):
             return self.mappings[key]
         else:
             return None
-        
-    def setProperty(self, key, value):
-        if(self.mappings.has_key(key)):
+
+    def set_property(self, key, value):
+        if self.mappings.has_key(key):
             self.mappings[key] = value
-            
+
     def store(self):
-        if(not os.path.isfile(self.path)):
+        if not os.path.isfile(self.path):
             raise IOError("The path that you specified is not a file.")
         else:
-            lKeyValue = []
+            lkey_value = []
             for key, value in self.mappings.iteritems():
-                lKeyValue.append(key + '=' + value + '\n')
+                lkey_value.append(key + '=' + value + '\n')
             f = open(self.path, 'wb')
-            f.writelines(lKeyValue)
+            f.writelines(lkey_value)
             f.flush()
             f.close()
-                    
-        
